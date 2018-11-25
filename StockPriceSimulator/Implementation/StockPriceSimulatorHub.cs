b69abd5b348377faa0fe52d1b0fe1c107b9b9b30
sandeepmulia <sandeepmulia@users.noreply.github.com>
2018-11-26 07:13:12 +1100

    Initial commit

---
+++ StockPriceSimulator/Implementation/StockPriceSimulatorHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace StockPriceSimulator.Implementation
+{
+    public class StockPriceSimulatorHub : Hub
+    {
+        private readonly StockTickerSimulatorImpl _stockTickerImpl;
+
+
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ StockPriceSimulator/Model/Stock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockPriceSimulator.Model
+{
+    public class Stock
+    {
+        private decimal _last;
+        private decimal _bid;
+        private decimal _ask;
+
+
+        public string Symbol { get; set; }
+
+        /// <summary>
+        /// Last Price
+        /// </summary>
+        public decimal Last { get; set; }
+
+        /// <summary>
+        /// Bid Price
+        /// </summary>
+        public decimal Bid { get; set; }
+
+        /// <summary>
+        /// Ask Price
+        /// </summary>
+        public decimal Ask { get; set; }
+
+        /// <summary>
+        /// Close (previous day) price
+        /// </summary>
+        public decimal Close { get; set; }
+
+    }
+}
